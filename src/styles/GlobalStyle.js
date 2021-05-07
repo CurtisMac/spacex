4d36dcb3ce56f65651e4ab246951f14ab26a7b72
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2021-05-07 21:13:28 +0100

    adds fonts

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,9 +1,72 @@
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 
+import BGBoldWoff2 from "../assets/font/BrandonGrotesque-Bold.woff2";
+import BGBoldWoff from "../assets/font/BrandonGrotesque-Bold.woff";
+import BGBoldTtf from "../assets/font/BrandonGrotesque-Bold.ttf";
+import BGRegularWoff2 from "../assets/font/BrandonGrotesque-Regular.woff2";
+import BGRegularWoff from "../assets/font/BrandonGrotesque-Regular.woff";
+import BGRegularTtf from "../assets/font/BrandonGrotesque-Regular.ttf";
+import BGRegularItalicWoff2 from "../assets/font/BrandonGrotesque-RegularItalic.woff2";
+import BGRegularItalicWoff from "../assets/font/BrandonGrotesque-RegularItalic.woff";
+import BGRegularItalicTtf from "../assets/font/BrandonGrotesque-RegularItalic.ttf";
+import BGBlackWoff2 from "../assets/font/BrandonGrotesque-Black.woff2";
+import BGBlackWoff from "../assets/font/BrandonGrotesque-Black.woff";
+import BGBlackTtf from "../assets/font/BrandonGrotesque-Black.ttf";
+import BGMediumWoff2 from "../assets/font/BrandonGrotesque-Medium.woff2";
+import BGMediumWoff from "../assets/font/BrandonGrotesque-Medium.woff";
+import BGMediumTtf from "../assets/font/BrandonGrotesque-Medium.ttf";
+
 const GlobalStyle = createGlobalStyle`
   ${reset}
-  /* other styles */
+  @font-face {
+    font-family: 'Brandon Grotesque';
+    src: url(${BGBoldWoff2}) format('woff2'),
+      url(${BGBoldWoff}) format('woff'),
+      url(${BGBoldTtf}) format('truetype');
+    font-weight: bold;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'Brandon Grotesque';
+    src: url(${BGRegularWoff2}) format('woff2'),
+      url(${BGRegularWoff}) format('woff'),
+      url(${BGRegularTtf}) format('truetype');
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'Brandon Grotesque';
+    src: url(${BGRegularItalicWoff2}) format('woff2'),
+      url(${BGRegularItalicWoff}) format('woff'),
+      url(${BGRegularItalicTtf}) format('truetype');
+    font-weight: normal;
+    font-style: italic;
+  }
+
+  @font-face {
+    font-family: 'Brandon Grotesque';
+    src: url(${BGBlackWoff2}) format('woff2'),
+      url(${BGBlackWoff}) format('woff'),
+      url(${BGBlackTtf}) format('truetype');
+    font-weight: 900;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'Brandon Grotesque';
+    src: url(${BGMediumWoff2}) format('woff2'),
+      url(${BGMediumWoff}) format('woff'),
+      url(${BGMediumTtf}) format('truetype');
+    font-weight: 500;
+    font-style: normal;
+  }
+
+  body {
+    font-family: 'Brandon Grotesque', Arial, Helvetica, sans-serif;
+  }
 `;
 
 export default GlobalStyle;
