deb2b703934289075ae728bc12b2532bef59f0db
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2021-05-08 23:33:03 +0100

    adds breakpoints to theme

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -3,6 +3,10 @@ const theme = {
   fontColor: "#545454",
   buttonFontColor: "#fff",
   fontFamily: `'Brandon Grotesque', Arial, Helvetica, sans-serif`,
+  xs: "380px",
+  sm: "640px",
+  md: "768px",
+  lg: "1024px",
 };
 
 export default theme;
