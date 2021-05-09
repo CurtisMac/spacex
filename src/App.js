e8a474c55422b43b02e68bef8ba9184ed7017290
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2021-05-09 21:35:55 +0100

    sets up state in App component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from "react";
-import GlobalStyle from "./styles/GlobalStyle";
+import React, { useReducer, useEffect, useState } from "react";
 import { ThemeProvider } from "styled-components";
+import GlobalStyle from "./styles/GlobalStyle";
 import theme from "./styles/theme";
+import { initialState, launchesReducer } from "./reducers/launchesReducer";
 
 //components
 import Header from "./components/header";
 
 const App = () => {
+  const [state, dispatch] = useReducer(launchesReducer, initialState);
+  const [error, setError] = useState(null);
   return (
     <React.Fragment>
       <ThemeProvider theme={theme}>
