dbe57e4458c4fbec2aa7094c23432f0309fb06b7
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2021-05-09 02:07:19 +0100

    imports Header into App

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import GlobalStyle from "./styles/GlobalStyle";
 import { ThemeProvider } from "styled-components";
 import theme from "./styles/theme";
 
+//components
+import Header from "./components/header";
 
 const App = () => {
   return (
     <React.Fragment>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
+        <Header />
       </ThemeProvider>
     </React.Fragment>
   );
