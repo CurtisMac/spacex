4fddce9e9d7311ab0f3024d3dc177c57f6418628
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2021-05-09 02:04:55 +0100

    Logo component is more responsive

--- src/components/header/Logo.js
+++ src/components/header/Logo.js
@@ -6,27 +6,29 @@ import logo from "../../assets/logo.png";
 
 //styles
 const StyledLogo = styled.div`
-    margin: 0 40px;
-    width: 100%;
+  margin: 0 40px;
+  @media (max-width: ${(props) => props.theme.sm}) {
+    margin: 0 10px 0 30px;
+  }
 `;
 const Image = styled.img`
-    max-width: 180px;
-    @media (max-width: ${(props) => props.theme.xs}) {
-        margin-left: -10px;
-    }
+  max-width: 180px;
+  @media (max-width: ${(props) => props.theme.sm}) {
+    margin-left: -10px;
+  }
 `;
 const LogoText = styled.span`
-    font-size: 1.2em;
-    margin-left: -10px;
+  font-size: 1.2em;
+  margin-left: -10px;
 `;
 
 const Logo = () => {
-    return (
-        <StyledLogo role="img">
-            <Image src={logo} alt="Logo" />
-            <LogoText>LAUNCHES</LogoText>
-        </StyledLogo>
-    );
+  return (
+    <StyledLogo role="img">
+      <Image src={logo} alt="Logo" />
+      <LogoText>LAUNCHES</LogoText>
+    </StyledLogo>
+  );
 };
 
 export default Logo;
