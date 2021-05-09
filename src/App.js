fb52d68d564232fb98558ec34715c6cf81949a1a
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2021-05-09 21:36:40 +0100

    App component loads launch data on load

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./styles/GlobalStyle";
 import theme from "./styles/theme";
 import { initialState, launchesReducer } from "./reducers/launchesReducer";
+import api from "./adapters/launchesApi";
 
 //components
 import Header from "./components/header";
@@ -10,6 +11,17 @@ import Header from "./components/header";
 const App = () => {
   const [state, dispatch] = useReducer(launchesReducer, initialState);
   const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await api.getAllData().catch((err) => {
+        setError("Network error");
+      });
+      dispatch({ type: "load", payload: data });
+    }
+    fetchData();
+  }, []);
+
   return (
     <React.Fragment>
       <ThemeProvider theme={theme}>
