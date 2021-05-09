124a2518fab75bf2e451a9294a70d9f9670455cd
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2021-05-09 21:32:26 +0100

    adds launchesReducer.js

---
+++ src/reducers/launchesReducer.js
@@ -0,0 +1,31 @@
+import update from "immutability-helper";
+
+const initialState = {
+  launches: [],
+  filterByYear: null,
+  order: "asc",
+  launchYears: [],
+};
+
+function launchesReducer(state, { type, payload }) {
+  if (type === "load") {
+    const { launches, rockets } = payload;
+    const data = launches.reduce(
+      (acc, cur) => {
+        const launchYear = new Date(cur.date_local).getFullYear();
+        cur.rocketDetails = rockets.find((rocket) => rocket.id === cur.rocket);
+        cur.launchYear = launchYear;
+        acc.years.add(launchYear);
+        acc.launches.push(cur);
+        return acc;
+      },
+      { years: new Set([]), launches: [] }
+    );
+    return update(state, {
+      launches: { $set: data.launches },
+      launchYears: { $set: Array.from(data.years) },
+    });
+  }
+}
+
+export { initialState, launchesReducer };
