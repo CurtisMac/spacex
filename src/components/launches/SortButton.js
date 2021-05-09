cdaccdf09f5be1f7d4a26feab74a883fa3300538
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2021-05-10 00:01:00 +0100

    adds SortButton component

---
+++ src/components/launches/SortButton.js
@@ -0,0 +1,46 @@
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+//components
+import Button from "../StyledButton";
+
+//assets
+import img from "../../assets/icon/sort.png";
+import img2x from "../../assets/icon/sort@2x.png";
+import img3x from "../../assets/icon/sort@3x.png";
+
+//styles
+const StyledButton = styled(Button)`
+  width: 160px;
+  .inverted {
+    transform: rotate(180deg);
+    transition: 1s ease-in-out;
+  }
+  .btn_icon {
+    transition: 0.4s ease-in-out;
+  }
+`;
+
+const SortButton = ({ order, dispatch }) => {
+  const handleClick = () => {
+    const newOrder = order === "asc" ? "desc" : "asc";
+    dispatch({ type: "sort", payload: newOrder });
+  };
+  const buttonText = order === "asc" ? "Sort Descending" : "Sort Ascending    ";
+  return (
+    <StyledButton onClick={handleClick}>
+      <span className="btn_text">{buttonText}</span>
+      <img
+        className={order === "asc" ? "btn_icon" : "btn_icon inverted"}
+        srcSet={`${img}, ${img2x} 2x, ${img3x} 3x`}
+      />
+    </StyledButton>
+  );
+};
+
+SortButton.propTypes = {
+  order: PropTypes.oneOf(["asc", "desc"]).isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
+export default SortButton;
