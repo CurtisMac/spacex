1560576c0e8b79f0f0b9e60b6327d89ccc03faee
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2021-05-10 00:00:04 +0100

    adds StyledButton component

---
+++ src/components/StyledButton.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  background-color: ${(props) => props.theme.primaryBlue};
+  color: ${(props) => props.theme.buttonFontColor};
+  padding: 5px 15px;
+  .btn_icon {
+    vertical-align: middle;
+    margin-left: 8px;
+  }
+  .btn_text {
+    vertical-align: middle;
+  }
+`;
+
+export default StyledButton;
