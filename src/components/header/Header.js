c55a892b21fae145de1f1d99a5fcb8758116acd8
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2021-05-09 02:06:43 +0100

    adds Header component

---
+++ src/components/header/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+
+//components
+import Button from "./Button";
+import Logo from "./Logo";
+
+//styles
+const StyledHeader = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  margin: 30px 0 10px;
+`;
+
+const Header = () => {
+  return (
+    <StyledHeader>
+      <Logo />
+      <Button />
+    </StyledHeader>
+  );
+};
+
+export default Header;
