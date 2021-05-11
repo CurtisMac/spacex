c56356cc182bce47b14b3949e99148a41c2ae6cf
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2021-05-11 01:33:50 +0100

    adds shadowColor to theme

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -2,6 +2,7 @@ const theme = {
   primaryBlue: "#215184",
   fontColor: "#545454",
   buttonFontColor: "#fff",
+  shadowColor: "#979797",
   fontFamily: `'Brandon Grotesque', Arial, Helvetica, sans-serif`,
   xs: "380px",
   sm: "640px",
